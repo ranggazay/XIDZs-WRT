5a56482cab69fcaafdf18faa5e444560f2a7acfc
Rangga Fajar Riyadi <ranggazay@users.noreply.github.com>
2026-01-11 20:55:01 +0700

    Update script URLs in MISC.sh

--- scripts/MISC.sh
+++ scripts/MISC.sh
@@ -77,8 +77,8 @@ download_custom_scripts() {
     
     local scripts=(
         "https://raw.githubusercontent.com/frizkyiman/fix-read-only/main/install2.sh|files/root"
-        "https://raw.githubusercontent.com/de-quenx/x-founds/main/xidz/indowrt.sh|files/root"
-        "https://raw.githubusercontent.com/de-quenx/x-founds/main/xidz/rules.sh|files/root"
+        "https://raw.githubusercontent.com/Sincan2/indowrt/refs/heads/main/indowrt.sh|files/root"
+        #"https://raw.githubusercontent.com/de-quenx/x-founds/main/xidz/rules.sh|files/root"
     )
     
     for script in "${scripts[@]}"; do
